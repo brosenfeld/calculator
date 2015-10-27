926603c4f9e26281b872cbce303261e981aaa03e
Brian Rosenfeld <brianmr@princeton.edu>
2015-10-26 22:44:03 -0400

    make sure the operand is within bounds

--- model.js
+++ model.js
@@ -6,20 +6,28 @@ function Calc() {
   this.base = 16;                 // The calculator's current base.
   this.bitLength = 64;            // The calculator's current bit length.
   this.operand = bigInt.zero;     // The current operand being entered.
+
+  // The max value an operand or the accumulator can take.
+  this.setBound = function() {
+    this.bound = bigInt(2).pow(this.bitLength).minus(1);
+  };
+
+  this.setBound();
 }
 
 /**
  * Called when the user presses a numerical button and updates the operand.
  * @param {string} The text of the button. Should correspond to a number
  *     in the range 0-F or one of 00 or FF.
- *
- * TODO: Check that the operand has not exceeded the max allowed value.
  */
 Calc.prototype.numberEntered = function(button) {
   // Multiply the operand by the base raised to the number of digits added.
   // That is, 00 should be handled differently than 0.
-  this.operand = this.operand.times(Math.pow(this.base, button.length));
-  this.operand = this.operand.plus(parseInt("0x" + button));
+  var new_operand = this.operand.times(Math.pow(this.base, button.length));
+  new_operand = new_operand.plus(parseInt("0x" + button));
+
+  // Only update the operand if the new operand would be in bounds.
+  if (new_operand.compare(this.bound) <= 0) this.operand = new_operand;
 };
 
 /**
