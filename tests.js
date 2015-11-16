390754ef12d41e95a6aae7620ec415644b7d81f8
Brian Rosenfeld <brianmr@princeton.edu>
2015-11-15 23:07:21 -0500

    set up unit tests and wrote tests for entering numbers

---
+++ tests.js
@@ -0,0 +1,107 @@
+function setup(base, representsBits, length, isSigned) {
+  var calc = new Calc();
+  calc.setBase(base, representsBits);
+  calc.setBitLength(length);
+  calc.setIsSigned(isSigned);
+  return calc;
+}
+
+function enterNumbers(calc, numbers) {
+  for (var i = 0; i < numbers.length; i++) {
+    calc.numberEntered(numbers[i]);
+  }
+}
+
+QUnit.test("Initial conditions", function(assert) {
+  var calc = new Calc();
+  assert.ok(calc.hasOperand, "Calc has operand");
+  assert.equal(calc.operand, bigInt.zero, "Operand is zero");
+  assert.equal(calc.accumulator, bigInt.zero, "Accumulator is zero");
+});
+
+QUnit.module("Entering numbers");
+QUnit.test("Entering digits", function(assert) {
+  var calc = setup(10, false, 64, true);
+  enterNumbers(calc, ["1", "2", "3", "4"]);
+  assert.equal(calc.operand.valueOf(), 1234, "Passed");
+
+  calc = setup(10, false, 64, true);
+  enterNumbers(calc, ["1", "0", "3", "4"]);
+  assert.equal(calc.operand.valueOf(), 1034, "Passed");
+
+  calc = setup(10, false, 64, true);
+  enterNumbers(calc, ["0", "0", "3"]);
+  assert.equal(calc.operand.valueOf(), 3, "Passed");
+});
+
+QUnit.test("Entering digits on negative number", function(assert) {
+  var calc = setup(10, false, 64, true);
+  calc.operand = bigInt(-1);
+  enterNumbers(calc, ["0", "1"]);
+  assert.equal(calc.operand.valueOf(), -101, "Passed");
+});
+
+QUnit.test("Entering digit out of range", function(assert) {
+  var calc = setup(10, false, 64, true);
+  enterNumbers(calc, ["1", "0", "11", "-1", "0"]);
+  assert.equal(calc.operand.valueOf(), 100, "Passed");
+});
+
+QUnit.test("Entering bits", function(assert) {
+  var calc = setup(16, true, 64, true);
+  enterNumbers(calc, ["FF"]);
+  assert.equal(calc.operand.valueOf(), 255, "Passed");
+
+  calc = setup(16, true, 64, true);
+  enterNumbers(calc, ["1", "00"]);
+  assert.equal(calc.operand.valueOf(), 256, "Passed");
+
+  calc = setup(2, true, 64, true);
+  enterNumbers(calc, ["1", "0", "0", "1"]);
+  assert.equal(calc.operand.valueOf(), 9, "Passed");
+});
+
+QUnit.test("Entering digit out of range", function(assert) {
+  var calc = setup(2, true, 64, true);
+  enterNumbers(calc, ["1", "F", "0"]);
+  assert.equal(calc.operand.valueOf(), 2, "Passed");  
+});
+
+QUnit.test("Mixing digits and bits and changing base", function(assert) {
+  var calc = setup(10, false, 64, true);
+  enterNumbers(calc, ["1", "5"]);
+  calc.setBase(16, true);
+  enterNumbers(calc, ["2"]); // F2
+  assert.equal(calc.operand.valueOf(), 242, "Passed"); 
+});
+
+// Hitting unsigned limit.
+QUnit.test("Hitting unsigned limit", function(assert) {
+  var calc = setup(10, false, 8, false);
+  enterNumbers(calc, ["2", "5", "6"]); // Shouldn't be able to put the 6 in.
+  assert.equal(calc.operand.valueOf(), 25, "Passed"); 
+});
+
+// Bits becoming negative number.
+QUnit.test("Above signed upper bound becomes negative", function(assert) {
+  var calc = setup(16, true, 8, true);
+  enterNumbers(calc, ["F", "F"]);
+  assert.equal(calc.operand.valueOf(), -1, "Passed");
+});
+
+// Hitting unsigned limit.
+QUnit.test("Hitting signed limits", function(assert) {
+  var calc = setup(10, false, 8, true);
+  enterNumbers(calc, ["1", "2", "9"]); // Shouldn't be able to put the 0 in.
+  assert.equal(calc.operand.valueOf(), 12, "Passed upper limit"); 
+
+  var calc = setup(10, false, 8, true);
+  calc.operand = bigInt(-1);
+  enterNumbers(calc, ["2", "9"]); // Shouldn't be able to put the 0 in.
+  assert.equal(calc.operand.valueOf(), -12, "Passed lower limit"); 
+});
+
+QUnit.module("Entering operations");
+QUnit.module("Binary operations");
+QUnit.module("Unary operations");
+QUnit.module("Changing modes");
